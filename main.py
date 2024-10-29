cfdf2916700147df134a0b5cb5eb51768a91a2ac
uw000 <gojw@kakao.com>
2024-10-29 10:36:54 +0900

    company component added

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ import streamlit as st
 
 st.title("MachineLearning")
 st.header("주식 예측 프로그램")
+
+# Streamlit 애플리케이션 제목
+st.title("기업 이름 입력")
+
+# 사용자로부터 기업 이름을 입력받기
+company_name = st.text_input("기업 이름을 입력하세요:")
